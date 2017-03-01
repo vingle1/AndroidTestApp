8631b53d0816001f2dff4488da158eb5edcd240a
vaibhav ingle <vingle@unomaha.edu>
2017-02-28 19:47:33 -0600

    Added radio group
    Added and changed the radio group to be dynamically oriented.

--- app/src/main/java/com/vogella/android/testapp/MainActivity.java
+++ app/src/main/java/com/vogella/android/testapp/MainActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.provider.SyncStateContract;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.util.Log;
 import android.widget.EditText;
-
+import android.widget.RadioGroup;
 
 
 public class MainActivity extends Activity {
@@ -16,6 +17,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RadioGroup group1 = (RadioGroup) findViewById(R.id.orientation);
+        group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.horizontal:
+                        group.setOrientation(LinearLayout.HORIZONTAL);
+                        break;
+                    case R.id.vertical:
+                        group.setOrientation(LinearLayout.VERTICAL);
+                        break;
+                }
+            }
+        });
+
     }
 
     public void onClick(View view) {
@@ -23,5 +40,6 @@ public class MainActivity extends Activity {
         String string = input.getText().toString();
         Toast.makeText(this, string, Toast.LENGTH_LONG).show();
     }
-
 }
+
+
