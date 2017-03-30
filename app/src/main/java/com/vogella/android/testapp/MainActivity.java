9849bd62b78ac1800955d3275a82d1263cbcb477
vaibhav ingle <vingle@unomaha.edu>
2017-03-30 14:40:31 -0500

    Added spinner

--- app/src/main/java/com/vogella/android/testapp/MainActivity.java
+++ app/src/main/java/com/vogella/android/testapp/MainActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.provider.SyncStateContract;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.util.Log;
 import android.widget.EditText;
@@ -22,6 +24,7 @@ public class MainActivity extends Activity {
         group1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
+
                 switch (checkedId) {
                     case R.id.horizontal:
                         group.setOrientation(LinearLayout.HORIZONTAL);
@@ -33,6 +36,15 @@ public class MainActivity extends Activity {
             }
         });
 
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ides_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 
     public void onClick(View view) {
@@ -41,5 +53,3 @@ public class MainActivity extends Activity {
         Toast.makeText(this, string, Toast.LENGTH_LONG).show();
     }
 }
-
-
